71b72572bc89fbaa7f6d0cafd3d6f3888aee878c
fuster <fusti92@gmailc.om>
2020-11-29 21:29:58 +0100

    Test9 Pasado

--- servistas/internal/integracion_test/integracion_test.go
+++ servistas/internal/integracion_test/integracion_test.go
@@ -1,7 +1,9 @@
 package integracion_test
 
 import (
+	"encoding/gob"
 	"fmt"
+	"net"
 	"servistas/internal/cltssh"
 	"servistas/internal/gvcomun"
 	"servistas/internal/msgsys"
@@ -32,7 +34,7 @@ const (
 
 	// PATH de los (plural) ejecutables de modulo golang de servicio de vistas
 	PATH = "/home/fuster/Descargas/Practica4/CodigoEsqueleto/servistas/cmd/"
-
+	// GO = /usr/local/go/bin/go
 	// fichero main de ejecutables relativos a PATH previo
 	EXECGV      = "cmdsrvvts/main.go " + NODOGV // un parámetro
 	EXECTESTCLT = "testcltvts/main.go "         // 2 parámetros en llamadas ssh
@@ -56,7 +58,7 @@ func TestPrimerasPruebas(t *testing.T) { // (m *testing.M) {
 	// Crear servidor de test y
 	//procesos en maquinas remotas : servidor de vistas
 	ts := startTestServer(NODOTEST)
-	ts.startDistributedProcesses()
+	ts.startDistributedProcesses(t)
 
 	// Run test sequence
 
@@ -84,13 +86,27 @@ func TestPrimerasPruebas(t *testing.T) { // (m *testing.M) {
 	t.Run("PromocionEsperaCopia:T6",
 		func(t *testing.T) { ts.PromocionEsperaCopiaTest6(t) })
 
+	// Test7: Primario manda latido 0 y es puesto en nodos de espera
 	t.Run("PrimarioRearrancaEspera:T7",
 		func(t *testing.T) {
 			ts.PrimarioAEsperaTest7(t)
 		})
+
+	// Test8: GV espera a que primario confirme la vista
+	t.Run("ServidorEsperaConfirmacion:T8",
+		func(t *testing.T) {
+			ts.EsperaConfirmacionTest8(t)
+		})
+
+	// Test9: Un nuevo servidor sin inicializar no puede ser primario
+	t.Run("ServidorSinInicializarNoEsPrimario:T9",
+		func(t *testing.T) {
+			ts.ServidorSinInicializarNoEsPrimarioTest9(t)
+		})
+
 	// tear down code
 	// eliminar procesos en máquinas remotas
-	ts.stopDistributedProcesses()
+	ts.stopDistributedProcessesNoErr(t)
 
 	ts.stop()
 }
@@ -126,8 +142,8 @@ func (ts *testServer) stop() {
 	}
 }
 
-func (ts *testServer) startDistributedProcesses() {
-
+func (ts *testServer) startDistributedProcesses(t *testing.T) {
+	t.Helper()
 	// Poner en marcha servidor/gestor de vistas y 3 clientes
 	// en 4 máquinas remota con ssh
 	cltssh.ExecMutipleHosts(SRVVTSCMD,
@@ -145,13 +161,27 @@ func (ts *testServer) startDistributedProcesses() {
 }
 
 func (ts *testServer) stopDistributedProcesses() {
-
 	// Parar procesos distribuidos con ssh
 	// una opción :
 	ts.Send(NODOGV, gvcomun.MsgFin{})
 	ts.Send(NODOCLIENTE1, gvcomun.MsgFin{})
 	ts.Send(NODOCLIENTE2, gvcomun.MsgFin{})
 	ts.Send(NODOCLIENTE3, gvcomun.MsgFin{})
+
+	// esperar parada se servidores remotos el tiempo suficiente
+	// para volcar salida de ejecuciones ssh en cmdOutput
+	time.Sleep(100 * time.Millisecond)
+}
+
+func (ts *testServer) stopDistributedProcessesNoErr(t *testing.T) {
+	t.Helper()
+	// Parar procesos distribuidos con ssh
+	// una opción :
+	// Se ignoran los posibles errores en el send
+	_ = ts.SendConError(NODOGV, gvcomun.MsgFin{})
+	_ = ts.SendConError(NODOCLIENTE1, gvcomun.MsgFin{})
+	_ = ts.SendConError(NODOCLIENTE2, gvcomun.MsgFin{})
+	_ = ts.SendConError(NODOCLIENTE3, gvcomun.MsgFin{})
 
 	// esperar parada se servidores remotos el tiempo suficiente
 	// para volcar salida de ejecuciones ssh en cmdOutput
@@ -171,7 +201,8 @@ func (ts *testServer) ningunPrimarioTest1(t *testing.T) {
 		gvcomun.ANSWERWAITTIME*time.Millisecond)
 
 	if !ok {
-		fmt.Printf("TIMEOUT SENDRECEIVE NINGUN PRIMERO TEST: %s", NODOCLIENTE1)
+		fmt.Printf(
+			"TIMEOUT SENDRECEIVE NINGUN PRIMERO TEST: %s", NODOCLIENTE1)
 		t.Fatalf(
 			"Ha saltado timeout esperando respuesta de Gestor de Vistas %#v",
 			t.Name())
@@ -219,7 +250,8 @@ func (ts *testServer) PrimerCopiaTest3(t *testing.T) {
 		gvcomun.ANSWERWAITTIME*time.Millisecond,
 	)
 	if !ok {
-		t.Fatal("Salta timeout esperando respuesta a latido -1 de cliente")
+		t.Fatal(
+			"Salta timeout esperando respuesta a latido -1 de cliente")
 	}
 
 	// Segundo cliente por primera vez:
@@ -248,9 +280,10 @@ func (ts *testServer) CopiaTomaRelevoTest4(t *testing.T) {
 	vTentativa = ts.clienteLatido(t, NODOCLIENTE1, 2)
 
 	for i := 0; i <= gvcomun.LATIDOSFALLIDOS; i += 1 {
-		// Mandar latidos de s2 durante 250 milis hasta que el GV de por muerto a primario
+		// Mandar latidos de NODO2 durante 250 milis
+		// hasta que el GV de por muerto a primario -> NODO1
 
-		time.Sleep(time.Millisecond * 50)
+		time.Sleep(time.Millisecond * gvcomun.INTERVALOLATIDOS)
 		vTentativa = ts.clienteLatido(t, NODOCLIENTE2, vTentativa.NumVista)
 	}
 	fmt.Printf("--------------------- %v\n", vTentativa)
@@ -294,9 +327,10 @@ func (ts *testServer) PromocionEsperaCopiaTest6(t *testing.T) {
 
 	// Configuración -> {numVista = 4, Prim: NODO2, Cop: NODO1}
 	for i := 0; i <= gvcomun.LATIDOSFALLIDOS; i += 1 {
-		// Mandar latidos de N1 y N3 durante 250 milis hasta que el GV de por muerto a primario
+		// Mandar latidos de N1 y N3 durante 250 milis
+		// hasta que el GV de por muerto a primario -> NODO2
 
-		time.Sleep(time.Millisecond * 50)
+		time.Sleep(time.Millisecond * gvcomun.INTERVALOLATIDOS)
 		ts.clienteLatido(t, NODOCLIENTE1, 4)
 		vistaN3 = ts.clienteLatido(t, NODOCLIENTE3, 4)
 	}
@@ -335,28 +369,106 @@ func (ts *testServer) PrimarioAEsperaTest7(t *testing.T) {
 	fmt.Println(".............", t.Name(), "Superado")
 }
 
-func (ts *testServer) clienteLatido0(t *testing.T,
-	nodoCliente msgsys.HostPuerto) gvcomun.Vista {
+// Nuevo servidor primario no confirma la vista
+func (ts *testServer) EsperaConfirmacionTest8(t *testing.T) {
+	// Partimos de la configuración del test 7
+	// {numVista = 6, Prim: NODO3, Cop: NODO2} <- Tentativa
+	// {numVista = 5, Prim: NODO1, Cop: NODO3} <- Valida
 
-	// solo nos interesa la vista tentativa devuelta por latido a Gestor Vistas
-	m, ok := ts.SendReceive(nodoCliente,
-		gvcomun.MsgLatido{0, nodoCliente},
+	// Hasta que Nodo3 no mande latido con 6, no se confirma
+	if ts.VistasSonConsistentes(t, 5) {
+		t.Fatal("Vista valida y tentativa son iguales antes de la confirmacion")
+	}
+
+	// Comprobar que se mantiene el estado en el tiempo
+	for i := 0; i <= gvcomun.LATIDOSFALLIDOS; i += 1 {
+		// Mandar latidos de N1 y N3 durante 250 milis hasta que el GV de por muerto a primario
+
+		time.Sleep(time.Millisecond * gvcomun.INTERVALOLATIDOS)
+		ts.clienteLatido(t, NODOCLIENTE3, 5) // <-- Primario no confirma vista
+		ts.clienteLatido(t, NODOCLIENTE2, 6) // <-- Copia envía numVistaTentativa
+	}
+
+	if ts.VistasSonConsistentes(t, 5) {
+		t.Fatal("Vista valida y tentativa son iguales antes de la confirmacion")
+	}
+	// PRIMARIO CONFIRMA VISTA
+	ts.clienteLatido(t, NODOCLIENTE3, 6)
+
+	// Comprobar que se ha confirmado
+	if !ts.VistasSonConsistentes(t, 6) {
+		t.Fatal("Vista valida y tentativa son distintas después de la confirmacion")
+	}
+	fmt.Println(".............", t.Name(), "Superado")
+}
+
+// Comprobar que la vista tentativa y la valida son distintas
+func (ts *testServer) VistasSonConsistentes(t *testing.T, numVistaAnt int) bool {
+	// Lanzamos una peticion de vista valida y tentativa
+	vistaTentativa := ts.clienteLatido(t, NODOCLIENTE3, numVistaAnt)
+	msgVistaValida, ok := ts.SendReceive(NODOCLIENTE3,
+		gvcomun.MsgPeticionVistaValida{NODOCLIENTE3},
 		gvcomun.ANSWERWAITTIME*time.Millisecond,
 	)
 	if !ok {
-		t.Fatal("Salta timeout esperando latido 0 de cliente")
+		t.Fatal("Salta timeout esperando haciendo peticion de vista valida")
+	}
+	vistaValida, ok2 := msgVistaValida.(gvcomun.MsgVistaValida)
+	if !ok2 {
+		t.Fatal("No es un mensaje de vista valida")
 	}
 
-	switch x := m.(type) {
-	case gvcomun.MsgVistaTentativa:
-		return x.Vista // salida correcta
-	default:
-		t.Fatalf(t.Name(),
-			"Mensaje recibido INCORRECTO en primerPrimario: %#v", x)
+	// Comprobamos que no tengan el mismo número de vista
+	if vistaTentativa.NumVista == vistaValida.Vista.NumVista {
+		return true
 	}
+	return false
+}
 
-	// no debería llegar a ejecutarse, pero se pone por error compilacion
-	return gvcomun.Vista{}
+// Si anteriores servidores caen, un nuevo servidor no
+func (ts *testServer) ServidorSinInicializarNoEsPrimarioTest9(t *testing.T) {
+	// NODO1 se añade a espera
+	vista := ts.clienteLatido0(t, NODOCLIENTE1)
+	// Aseguramos que estamos en la vista {6, P:NODO3, C:NODO2}
+	vistasAcomparar{
+		recibido: vista,
+		referencia: gvcomun.Vista{
+			NumVista: 6,
+			Primario: NODOCLIENTE3,
+			Copia:    NODOCLIENTE2,
+		},
+		t: t,
+	}.comprobar()
+	var err error
+	for i := 0; i <= gvcomun.LATIDOSFALLIDOS; i += 1 {
+		// Mandar latidos de N1 hasta que PRIMARIO Y COPIA fallen
+		// Y por tanto el GV tambien caiga
+
+		time.Sleep(time.Millisecond * gvcomun.INTERVALOLATIDOS)
+		// Puede fallar cualquier latido, pero lo comprobamos tras el tiempo
+
+		err = ts.SendConError(NODOGV, gvcomun.MsgLatido{
+			NumVista:  vista.NumVista,
+			Remitente: NODOCLIENTE1,
+		})
+		if err != nil {
+			t.Logf("%v", err)
+			break
+		}
+	}
+	// Si no devuelve error el dialTCP, fallamos
+	if err != nil {
+		t.Log("Send ha dado error")
+	} else {
+		t.Fatal("El gestor de vistas ha contestado y debería estar caído")
+	}
+	fmt.Println(".............", t.Name(), "Superado")
+}
+
+func (ts *testServer) clienteLatido0(t *testing.T,
+	nodoCliente msgsys.HostPuerto) gvcomun.Vista {
+
+	return ts.clienteLatido(t, nodoCliente, 0)
 }
 
 func (ts *testServer) clienteLatido(t *testing.T,
@@ -412,4 +524,18 @@ func (vs vistasAcomparar) comprobar() {
 func (ts *testServer) lanzarNodo1() {
 	cltssh.ExecMutipleHosts(CLTVTSCMD+NODOCLIENTE1+" "+NODOGV+" "+NODOTEST,
 		[]string{MAQUINA3}, ts.cmdOutput, PRIVKEYFILE)
+}
+
+// Hace un send devolviendo un error en vez de hacer log.Fatal
+func (ts *testServer) SendConError(destinatario msgsys.HostPuerto, msg msgsys.Message) error {
+	conn, err := net.Dial("tcp", string(destinatario))
+	if err != nil {
+		return err
+	}
+	// fmt.Printf("Message for encoder: %#v \n", msg)
+	encoder := gob.NewEncoder(conn)
+	err = encoder.Encode(&msg)
+
+	_ = conn.Close()
+	return nil
 }
